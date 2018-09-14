810e0d683445b88acbbdba69da2332b77cb1e9f3
olegsherbakov <sherbakov.olegik@gmail.com>
2018-09-14 17:50:18 +0300

    v2

---
+++ assets/data.ts
@@ -0,0 +1,74 @@
+import { ICard } from '../app/definitions/interfaces'
+import { TStatus } from '../app/definitions/types'
+
+export const statuses: TStatus[] = [
+  'open',
+  'pending',
+  'dev',
+  'test',
+  'qa',
+  'done',
+]
+
+export const boards = [
+  {
+    status: <TStatus>'open',
+    title: 'Discussion',
+    elements: new Array<ICard>(),
+  },
+  {
+    status: <TStatus>'pending',
+    title: 'Pending',
+    elements: new Array<ICard>(),
+  },
+  {
+    status: <TStatus>'dev',
+    title: 'Developing',
+    elements: new Array<ICard>(),
+  },
+  {
+    status: <TStatus>'test',
+    title: 'Testing',
+    elements: new Array<ICard>(),
+  },
+  {
+    status: <TStatus>'qa',
+    title: 'Correct',
+    elements: new Array<ICard>(),
+  },
+  {
+    status: <TStatus>'done',
+    title: 'Done',
+    elements: new Array<ICard>(),
+  },
+]
+
+export const cards = [
+  <ICard>{
+    id: 1,
+    title: 'Card #1',
+    state: 'new',
+    status: <TStatus>'open',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    tags: ['tag#1'],
+  },
+  <ICard>{
+    id: 2,
+    title: 'Card #2',
+    state: 'error',
+    status: <TStatus>'open',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    tags: ['tag#1'],
+  },
+  <ICard>{
+    id: 3,
+    title: 'Card #3',
+    state: 'error',
+    status: <TStatus>'open',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    tags: ['tag#5', 'to match long tag #77567567'],
+  },
+]
